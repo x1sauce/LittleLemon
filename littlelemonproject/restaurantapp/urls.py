e6a069354b0239476cc99a6e883ece342924e2c7
x1sauce <shawnhuang.hsx@gmail.com>
2024-06-18 09:50:03 -0700

    Installed djoser, configured default renderer and default authentication classes in setting.py

--- littlelemonproject/restaurantapp/urls.py
+++ littlelemonproject/restaurantapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -11,7 +12,8 @@ urlpatterns = [
     # path('bookings/', views.bookingview.as_view(), name='bookings'),
     path('users/', views.UserViewSet.as_view({'get': 'list'})),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('menu/', views.MenuItemView.as_view(), name='menu'),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu-items/', views.MenuItemView.as_view(), name='menu'),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item'),
     path('booking/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token)
 ]
