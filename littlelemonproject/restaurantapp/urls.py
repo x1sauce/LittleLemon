f12be0f44ceda04271caff99547e66809a4f37de
x1sauce <shawnhuang.hsx@gmail.com>
2024-06-25 12:11:43 -0700

    Added base, menu, book, booking, header and footer templates. Modified Booking model

--- littlelemonproject/restaurantapp/urls.py
+++ littlelemonproject/restaurantapp/urls.py
@@ -5,15 +5,15 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('bookings/', views.bookingview.as_view(), name='bookings'),
     path('users/', views.UserViewSet.as_view({'get': 'list'})),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    path('reservations/', views.reservations, name="reservations"),
     path('menu-items/', views.MenuItemView.as_view(), name='menu'),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view(), name='menu-item'),
-    path('booking/', include(router.urls)),
+    path('bookings', views.bookings, name='bookings'),
     path('api-token-auth/', obtain_auth_token)
 ]
